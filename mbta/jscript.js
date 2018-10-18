8f5e6be8bf077058017a42e77e34e5b4c181df0e
Nick Camp <nickcamp@Nicks-MBP.fios-router.home>
2018-10-18 12:50:16 -0400

    added link between .html and .js

--- mbta/jscript.js
+++ mbta/jscript.js
@@ -1,1 +1,11 @@
 /* Here be javascript */
+/* access contol allow origin */
+/* check cross origin resource sharing to get around same origin policy */
+/* access-control-allow-origin */ /* this means the server can allow cross origin requests 8?
+/* the above is on the SERVER level so this is in assignment number 3 */
+var testDiv = document.getElementById('testID')
+
+
+function loadFunc(){
+  testDiv.innerHTML = "<h1>This is being created by javascript load function</h1>";
+}
