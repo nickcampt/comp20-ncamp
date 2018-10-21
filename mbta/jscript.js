35060887214556a64f9c7b5b850cac523f4367af
Nick Camp <nickcamp@Nicks-MBP.fios-router.home>
2018-10-21 01:13:28 -0400

    removed some unecessary comments

--- mbta/jscript.js
+++ mbta/jscript.js
@@ -53,11 +53,8 @@ function initMap() {
 
    var iconImageData = {
         url: 'https://cdn130.picsart.com/272354380029211.png?c256x256',
-        // This marker is 20 pixels wide by 32 pixels high.
         scaledSize: new google.maps.Size(30, 30),
-        // The origin for this image is (0, 0).
         origin: new google.maps.Point(0, 0),
-        // The anchor for this image is the base of the flagpole at (0, 32).
         anchor: new google.maps.Point(15, 30)
     };
 
