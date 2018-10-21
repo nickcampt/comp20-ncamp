a1b95bba45812f587b2a477f8e7322300b16f6d8
Nick Camp <nickcamp@Nicks-MBP.fios-router.home>
2018-10-20 23:13:02 -0400

    added icon variables for red line stops

--- mbta/jscript.js
+++ mbta/jscript.js
@@ -67,4 +67,126 @@ function makeIcons() {
     title: 'Andrew'
   });
 
+  var portrIcon = new google.maps.Marker({
+    position: {lat: 42.3884, lng: -71.11914899999999},
+    map: map,
+    title: 'Porter Square'
+  });
+
+  var harsqIcon = new google.maps.Marker({
+    position: {lat: 42.373362, lng:  -71.118956},
+    map: map,
+    title: 'Harvard Square'
+  });
+
+
+  var jfkIcon = new google.maps.Marker({
+    position: {lat 42.320685: , lng: -71.052391},
+    map: map,
+    title: 'JFK/UMass'
+  });
+
+
+  var shmnlIcon = new google.maps.Marker({
+    position: {lat:  42.31129, lng: -71.053331},
+    map: map,
+    title: 'Savin Hill'
+  });
+
+  var pktrmIcon = new google.maps.Marker({
+    position: {lat: 42.35639457, lng: -71.0624242},
+    map: map,
+    title: 'Park Street'
+  });
+
+  var brdwyIcon = new google.maps.Marker({
+    position: {lat: 42.342622, lng: -71.056967},
+    map: map,
+    title: 'Broadway'
+  });
+
+  var nqncyIcon = new google.maps.Marker({
+    position: {lat: 42.275275, lng: -71.029583},
+    map: map,
+    title: 'North Quincy'
+  });
+
+  var smmnlIcon = new google.maps.Marker({
+    position: {lat: 42.29312583, lng: -71.06573796000001},
+    map: map,
+    title: 'Shawmut'
+  });
+
+  var davisIcon = new google.maps.Marker({
+    position: {lat: 42.39674, lng: -71.121815},
+    map: map,
+    title: 'Davis'
+  });
+
+  var alfclIcon = new google.maps.Marker({
+    position: {lat: 42.395428, lng: -71.142483},
+    map: map,
+    title: 'Alewife'
+  });
+
+  var knnclIcon = new google.maps.Marker({
+    position: {lat: 42.36249079, lng:  -71.08617653},
+    map: map,
+    title: 'Kendall/MIT'
+  });
+
+  var chmnlIcon = new google.maps.Marker({
+    position: {lat: 42.361166, lng: -71.070628},
+    map: map,
+    title: 'Charles/MGH'
+  });
+
+  var wnxgdIcon = new google.maps.Marker({
+    position: {lat: 42.355518, lng: -71.060225},
+    map: map,
+    title: 'Downtown Crossing'
+  });
+
+  var qnctrIcon = new google.maps.Marker({
+    position: {lat: 42.251809, lng:- 71.005409},
+    map: map,
+    title: 'Quincy Center'
+  });
+
+  var qamnlIcon = new google.maps.Marker({
+    position: {lat: 42.233391, lng: -71.007153},
+    map: map,
+    title: 'Quincy Adams'
+  });
+
+  var asmnlIcon = new google.maps.Marker({
+    position: {lat: 42.284652, lng: -71.06448899999999},
+    map: map,
+    title: 'Ashmont'
+  });
+
+  var wlstaIcon = new google.maps.Marker({
+    position: {lat: 42.2665139, lng: -71.0203369},
+    map: map,
+    title: 'Wollaston'
+  });
+
+  var fldcrIcon = new google.maps.Marker({
+    position: {lat: 42.300093, lng: -71.061667},
+    map: map,
+    title: 'Fields Corner'
+  });
+
+  var cntsqIcon = new google.maps.Marker({
+    position: {lat: 42.365486, lng: -71.103802},
+    map: map,
+    title: 'Central Square '
+  });
+
+  var brntnIcon = new google.maps.Marker({
+    position: {lat: 42.2078543, lng:  -71.0011385},
+    map: map,
+    title: 'Braintree'
+  });
+
 }
