7c5bd2979e648a5d0c954020a3d4f3cecbe323b8
Nick Camp <nickcamp@Nicks-MBP.fios-router.home>
2018-10-24 13:34:18 -0400

    added lat long variables for the stops so drawing polys and icons are easier

--- mbta/jscript.js
+++ mbta/jscript.js
@@ -6,17 +6,40 @@
 /* infoWindow is a bad variable name as we may have many ? */
 var map, infoWindow, localLat, localLong;
 var yourIcon, sstatIcon, andrwIcon, portrIcon, harsqIcon, jfkIcon, shmnlIcon, pktrmIcon, brdwyIcon, nqncyIcon, smmnlIcon, davisIcon, alfclIcon, knnclIcon, chmnlIcon, dwnxgIcon, qnctrIcon, asmnlIcon, wlstaIcon, fldcrIcon, cntsqIcon, brntnIcon;
-
+var sstatLl = [42.352271, -71.05524200000001];
+var andrwLl = [42.330154, -71.057655];
+var portrLl = [42.3884, -71.11914899999999];
+var harsqLl = [42.373362, -71.118956];
+var jfkLl = [42.320685, -71.052391];
+var shmnlLl = [42.31129, -71.053331];
+var pktrmLl = [42.35639457, -71.0624242];
+var brdwyLl = [42.342622, -71.056967];
+var nqncyLl = [42.275275, -71.029583];
+var smmnlLl = [42.29312583, -71.06573796000001];
+var davisLl = [42.39674, -71.121815];
+var alfclLl = [42.395428, -71.142483];
+var knnclLl = [42.36249079, -71.08617653];
+var chmnlLl = [42.361166, -71.070628];
+var dwnxgLl = [42.355518, -71.060225];
+var qnctrLl = [42.251809, -71.005409];
+var qamnlLl = [42.233391, -71.007153];
+var asmnlLl = [42.284652, -71.06448899999999];
+var wlstaLl = [42.2665139, -71.0203369];
+var fldcrLl = [42.300093, -71.061667];
+var cntsqLl = [42.365486, -71.103802];
+var brntnLl = [42.2078543, -71.0011385];
 
 function testJSON(data){
     var logString = data;
     console.log(logString);
+    console.log("yeey");
+
 }
 
 function testXMLHttoRequest(){
     console.log("requesting using stop_id=place-davis");
     var requestURL = "https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id=place-davis";
-    var request = new XMLHTttpRequest();
+    var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
@@ -89,7 +112,7 @@ function initMap() {
     };
 
     sstatIcon = new google.maps.Marker({
-        position: {lat: 42.352271, lng: 71.05524200000001 },
+        position: {lat: sstatLl[0], lng: sstatLl[1] },
         map: map,
         icon: iconImageData,
         shape: iconShapeData,
